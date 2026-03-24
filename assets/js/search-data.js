1ed82470e638b8f8224b4b3fe29e1f7d4774359f
a-lucic <31237441+a-lucic@users.noreply.github.com>
2026-03-24 10:21:33 +0000

    Deploying to gh-pages from @ xaiforscience/xaiforscience.github.io@94004a6b9d359652b48d43d70fb663bbbe38f7b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-people",
           title: "People",
-          description: "members of the lab or group",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
